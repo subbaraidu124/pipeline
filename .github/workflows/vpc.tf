79cffb32727cc2583c35bae0f74a11927da30225
subbaraidu124 <124096391+subbaraidu124@users.noreply.github.com>
2023-07-24 12:00:45 -0500

    Update vpc.tf

--- .github/workflows/vpc.tf
+++ .github/workflows/vpc.tf
@@ -1,4 +1,28 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      Version = "~>3.27"
+    }
+  }
+
+  required_version = ">=0.14.9"
+
+}
+provider "aws" {
+  version = "~>3.0"
+  region  = "east-us-1"
+}
+
+backend "s3" {
+       bucket = "[Remote_State_S3_Bucket_Name]"
+       key    = "[Remote_State_S3_Bucket_Key]"
+       region = "east-us-1"
+   }
+}
+
 resource "aws_vpc" "main" {
   cidr_block = var.cidr_block
 
 }
+
