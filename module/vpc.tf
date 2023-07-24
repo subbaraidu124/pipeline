f6eb175733655f9fe2e71516f1108d7d733728b8
subbaraidu124 <124096391+subbaraidu124@users.noreply.github.com>
2023-07-24 13:14:42 -0500

    Update vpc.tf

--- module/vpc.tf
+++ module/vpc.tf
@@ -1,4 +1,28 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      Version = "~>3.27"
+    }
+  }
+
+  required_version = ">=0.14.9"
+
+}
+
+provider "aws" {
+  version = "~>3.0"
+  region  = "east-us-1"
+}
+backend "s3" {
+       bucket = "subbaraidu"
+       key    = "test"
+       region = "east-us-1"
+   }
+}
+
+
 resource "aws_vpc" "main" {
   cidr_block = var.cidr_block
-
+region = "east-us-1"
 }
