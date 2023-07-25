90b7176417a4d421cfe525544c92f3727647dea8
N B V Subba raidu <subbaraidu124@gmail.orgg>
2023-07-25 14:24:43 -0500

    hello

--- module/subnet.tf
+++ module/subnet.tf
@@ -1,10 +1,10 @@
 provider "aws" {
   region = "us-east-1"  
 }
-# Create Public Subnet
+
 resource "aws_subnet" "public" {
   vpc_id            = "vpc-005c38307f2ac53e5"
-  cidr_block        = "10.0.1.0/24" # Replace with your desired public subnet CIDR block
+  cidr_block        = "10.0.1.0/24"
   map_public_ip_on_launch = true
 }
 
