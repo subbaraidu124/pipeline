1f0a5016ec02d71ad1673096ff41a52f3e9e46ba
N B V Subba raidu <subbaraidu124@gmail.orgg>
2023-07-25 14:28:03 -0500

    hello

--- module/vpc.tf
+++ module/vpc.tf
@@ -4,3 +4,8 @@ provider "aws" {
 resource "aws_vpc" "main" {
   cidr_block = "10.0.0.0/16"
 }
+resource "aws_subnet" "public" {
+  vpc_id            = "vpc-005c38307f2ac53e5"
+  cidr_block        = "10.0.1.0/24"
+  map_public_ip_on_launch = true
+}
